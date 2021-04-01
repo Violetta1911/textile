f89e4fc68c64b263ca0b429dc84e749a41a84e09
Violetta1911 <sva198366@gmail.com>
2021-04-01 19:00:35 +0300

    finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,20 @@
-$(function(){
+$(function () {
+  $(".products__slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+  });
 
-    $('.products__slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      dots: false,
-          });
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+  });
 
-          $(".js-range-slider").ionRangeSlider({
-            type: "double",
-            min: 0,
-            max: 1000,
-            from: 0
-            
-        });
-  
-  $('.gtbuilder__tab  .tab, .personal-info__tab .tab').on('click', function(event) {
-    var id = $(this).attr('data-id');
-      $('.gtbuilder__tab, .personal-info__tab').find('.tab-item').removeClass('active-tab').hide();
-      $('.gtbuilder__tab .tabs, .personal-info__tab .tabs').find('.tab').removeClass('active');
-      $(this).addClass('active');
-      $('#'+id).addClass('active-tab').fadeIn();
-      return false;
+  $('.blog').on('click', function (){
+    $('.submenu').slideToggle('')
     });
-     
 
-  var mixer = mixitup ('.products__inner-box');  
-  
 });
